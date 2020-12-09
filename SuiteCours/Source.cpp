f47424f13f38dd75315d064851540119873b7bae
RBasic <rcham.pro@gmail.com>
2020-12-09 13:00:32 +0100

    InsertSort Working + SelectSort TODO

--- SuiteCours/Source.cpp
+++ SuiteCours/Source.cpp
@@ -1,4 +1,4 @@
-#include <iostream>
+﻿#include <iostream>
 #include <ctime>
 
 using namespace std;
@@ -20,9 +20,34 @@ void bubbleSort(int tab[10]) {
 	}
 }
 
+void insertSort(int tab[10]) {
+	for(int i = 1; i < 10; i++) {
+		int x = tab[i];
+		int j = i;
+		while (j > 0 && tab[j - 1] > x) {
+			tab[j] = tab[j - 1];
+			j--;
+		}
+		tab[j] = x;
+	}
+}
+
+void selectionsort(int tab[10]) {
+
+	/*
+	n ← longueur(t)
+	pour i de 0 à n - 2
+		min ← i
+		pour j de i + 1 à n - 1
+			si t[j] < t[min], alors min ← j
+		fin pour
+		si min ≠ i, 
+			alors échanger t[i] et t[min]
+	fin pour
+	*/
+}
 
 int main() {
-
 	srand(time(NULL));
 
 	int tab[10] = { 0 };
@@ -40,15 +65,15 @@ int main() {
 	}
 
 	for (int i = 0; i < 10; i++) {
-		cout << "tab[" << i << "] = " << tab[i] << endl;
+		cout << "|" << tab[i];
 	}
-
-	cout << "BUBBLE SORT" << endl;
-	bubbleSort(tab);
-
-	/*for (int i = 0; i < 10; i++) {
-		cout << "tab[" << i << "] = " << tab[i] << endl;
-	}*/
+	cout << "|" << endl;
+	cout << "SELECT SORT" << endl;
+	selectionsort(tab);
+	for (int i = 0; i < 10; i++) {
+		cout << "|" << tab[i];
+	}
+	cout << "|" << endl;
 	
 	return 0;
 }
